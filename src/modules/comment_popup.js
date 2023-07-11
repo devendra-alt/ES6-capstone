d7bb66ff295e02cf646e1e1dae1a5f1afa709cb8
devendra-alt <devendramulewa171@gmail.com>
2023-07-11 20:04:52 +0530

    Added render popup for comment-popup

--- src/modules/comment_popup.js
+++ src/modules/comment_popup.js
@@ -1,3 +1,35 @@
+const renderCommentPopUp = (meal) => {
+  const popUpEl = document.createElement('div');
+  const mealImage = document.createElement('img');
+  const mealTitle = document.createElement('h1');
+  const exitBtnEl = document.createElement('img');
+
+  mealImage.src = new URL(meal.strMealThumb);
+  exitBtnEl.src = exit;
+
+  exitBtnEl.addEventListener('click', (e) => {
+    e.target.parentNode.remove();
+  });
+
+  mealTitle.innerText = meal.strMeal;
+
+  popUpEl.setAttribute('data-mealID', meal.idMeal);
+  mealImage.setAttribute('data-mealID', meal.idMeal);
+  mealTitle.setAttribute('data-mealID', meal.idMeal);
+
+  popUpEl.classList.add('comment-popup');
+  mealImage.classList.add('comment-popup-img');
+  mealTitle.classList.add('comment-popup-title');
+  exitBtnEl.classList.add('comment-popup-exit-btn');
+
+  popUpEl.appendChild(exitBtnEl);
+  popUpEl.appendChild(mealImage);
+  popUpEl.appendChild(mealTitle);
+  popUpEl.appendChild(createMealDetailsListEl(meal));
+
+  document.body.appendChild(popUpEl);
+};
+
 const commentPopUp = (id) => {
   const mealContainer = document.querySelector('.section');
   mealContainer.addEventListener('click', async (e) => {
