6ff8700105367f58a14a14db120979bf0b0343db
devendra-alt <devendramulewa171@gmail.com>
2023-07-11 20:03:46 +0530

    Added MealDB class

--- src/modules/mealDB.js
+++ src/modules/mealDB.js
@@ -1,3 +1,11 @@
+class MealDb {
+  #URL;
+  #LOOKUP;
+  constructor() {
+    this.#URL = `https://www.themealdb.com/api/json/v1/1/`;
+    this.#LOOKUP = `lookup.php?i=`;
+  }
+
   async lookUpMeal(id) {
     try {
       const res = await fetch(`${this.#URL}${this.#LOOKUP}${id}`, {
@@ -9,3 +17,14 @@
       console.log(error);
     }
   }
+
+  extractData = (data) => {
+    for (const key in data) {
+      if (Object.hasOwnProperty.call(data, key)) {
+        return data[key][0];
+      }
+    }
+  };
+}
+
+export default MealDb;
