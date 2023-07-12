b06ac0471800e192b02d71fdc31c846431c74bbc
devendra-alt <devendramulewa171@gmail.com>
2023-07-12 17:21:19 +0530

    Updated initComments to be dynamic

--- src/modules/comments/initComments.js
+++ src/modules/comments/initComments.js
@@ -1,12 +1,13 @@
 import MealDb from '../mealDB.js';
 import renderCommentPopUp from './comments.js';
 
-const initCommentPopUp = (id, involvement) => {
+const initCommentPopUp = (involvement) => {
   const mealContainer = document.querySelector('.section');
   mealContainer.addEventListener('click', async (e) => {
-    if (e.target.classList.contains('comment-btn')) {
+    if (e.target.classList.contains('meal-item-comment-btn')) {
       const mealDb = new MealDb();
-      const meal = await mealDb.lookUpMeal(id);
+      console.log(e.target.dataset);
+      const meal = await mealDb.lookUpMeal(e.target.dataset.mealid);
       renderCommentPopUp(meal, involvement);
     }
   });
