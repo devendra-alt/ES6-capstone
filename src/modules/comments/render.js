da7800b450829545dd1d753899ff364c582d7480
devendra-alt <devendramulewa171@gmail.com>
2023-07-12 09:03:35 +0530

    Added createCommentEL for intracting with Envolment API and creating comments list

--- src/modules/comments/render.js
+++ src/modules/comments/render.js
@@ -1,3 +1,32 @@
+const createCommentsEl = async (meal) => {
+  const commentSectionEl = document.createElement('div');
+  const commentSectionTitle = document.createElement('h1');
+  const commentDetailsList = document.createElement('ul');
+
+  commentSectionEl.classList.add('popup-comment-section');
+
+  commentSectionEl.setAttribute('data-comment-section-id', meal.idMeal);
+  commentSectionTitle.setAttribute('data-comment-section-id', meal.idMeal);
+  commentDetailsList.setAttribute('data-comment-section-id', meal.idMeal);
+
+  const envolment = new Envolment();
+  const commentsData = await envolment.getComments(meal.idMeal);
+
+  commentSectionTitle.innerText = `Comments(${commentsData.length})`;
+
+  commentsData.forEach((element) => {
+    const listItem = document.createElement('p');
+    listItem.classList.add('popup-comments-item');
+    listItem.innerText = `${element.creation_date} ${element.username} : ${element.comment}`;
+    commentDetailsList.appendChild(listItem);
+  });
+
+  commentSectionEl.appendChild(commentSectionTitle);
+  commentSectionEl.appendChild(commentDetailsList);
+
+  return commentSectionEl;
+};
+
 const renderCommentPopUp = async (meal) => {
   const popUpEl = document.createElement('div');
   const mealImage = document.createElement('img');
