fd40124f87b4c079c914955ea245fab64d282e6a
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-07-12 14:17:08 +0000

    Add

--- src/index.js
+++ src/index.js
@@ -3,18 +3,11 @@ import './styles/style.css';
 import './assets/meal_logo.jpg';
 import './styles/reservations.css';
 
-import PullMealData from '/Users/mrblue/Music/Microverse/Module 2/Projects/ES6-capstone/src/reservationsModal/pullMeal.js';
-
+import PullMealData from './reservationsModal/pullMeal.js';
 
 import Reservations from './reservationsModal/reservations.js';
 import fetchReservations from './reservationsModal/fetchResev.js';
 
-
 const reservationsModal = new Reservations();
 const getMealData = new PullMealData();
 const fetchedReservationData = new fetchReservations();
-
-
-
-    
-  
