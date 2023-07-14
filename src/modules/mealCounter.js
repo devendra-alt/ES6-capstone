296e046ffcd25eaa667b57ce8e4166766449788a
devendra-alt <devendramulewa171@gmail.com>
2023-07-14 17:55:00 +0530

    Updated meal website site name

--- src/modules/mealCounter.js
+++ src/modules/mealCounter.js
@@ -1,7 +1,7 @@
 const mealCounter = () => {
   const mealList = document.querySelector('.section');
   const navLinkEl = document.querySelectorAll('.nav-link');
-  navLinkEl[0].innerHTML = `Meal Time (${mealList.childNodes.length})`;
+  navLinkEl[0].innerHTML = `Sparkles De'Light (${mealList.childNodes.length})`;
 };
 
 export default mealCounter;
